ed53d76e0137757e9fcbedc761e4a1a8ae2887f0
Laura Tissi <laura.tracierra@gmail.com>
2021-05-05 09:55:38 -0300

    com contador e borda verde nas opções clicadas

--- src/pedido.js
+++ src/pedido.js
@@ -3,12 +3,27 @@ import React from 'react';
 export default function Pedido(props){
     const { imagem, prato, descricao, valor} = props;
     const [borda, setBorda] = React.useState(""); 
+    const [selecionado, setSelecionado] = React.useState("adicionar escondido"); 
+    const [contador, setContador] = React.useState(1); 
+
     return(
-        <div class={borda} onClick={()=> setBorda("borda-verde")}>
+        <div class={borda} onClick={Selecionado}>
             <img src = {imagem}></img>
             <p>{prato}</p>
             <p>{descricao}</p>
-            <p>{valor}</p>
+            <span>
+                <p>{valor}</p>
+                <div class={selecionado}>
+                    <span onClick={()=>setContador(contador -1)}>-</span>
+                    <span>{contador}</span>
+                    <span onClick={()=>setContador(contador +1)}>+</span>
+                </div>
+            </span>
         </div>
     )
+
+    function Selecionado() {
+        setBorda("borda-verde")
+        setSelecionado("adicionar")
+    };
 }
