6356b9e75ae466b4c74c96dbc509ee2d48436bf6
Laura Tissi <laura.tracierra@gmail.com>
2021-05-05 08:32:47 -0300

    botão inferior mudando de cor ao clicar e mudando o texto

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Topo from './topo'
 import Corpo from './corpo'
 import Base from './base'
+import React from 'react';
 
 export default function App(){
+
     return(
         <>
         <Topo/>
--- src/base.js
+++ src/base.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 export default function Base(){
     const [texto, setTexto] = React.useState("Selecione os 3 itens para fechar o pedido");
+    const [color, setColor] = React.useState('#CBCBCB');
+    React.useEffect(() => {document.querySelector(".base").children[0].style.backgroundColor = color;}, [color]);
     return(
         <div class="base">
-            <div onClick={()=>setTexto("Fechar pedido")}>
+            <div onClick={FecharPedido}>
                 <p>{texto}</p>
             </div>
         </div>
     )
+
+    function FecharPedido(){
+       // if(sobremesa !== null && bebida !== null && principal !== null){
+            setTexto("Fechar pedido");
+            setColor(color !== null?'#32B72F':'#CBCBCB')
+
+        //}
+    }
 }
